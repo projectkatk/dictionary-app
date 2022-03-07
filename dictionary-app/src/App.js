e58eb4b1d8e17ba9e5edf87031720697afac8647
Kate Kim <kay.k.projects@gmail.com>
2022-03-07 16:26:53 +1000

    bootstrap link updated

--- dictionary-app/src/App.js
+++ dictionary-app/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="app-logo">Dictionary</h1>
+        <h1 className="app-logo fw-bold">Dictionary</h1>
       </header>     
     </div>
   );
