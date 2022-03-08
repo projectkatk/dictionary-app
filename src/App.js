cd828f812418373d3c1ad4646af8f39348c9b13d
Kate Kim <kay.k.projects@gmail.com>
2022-03-08 23:54:24 +1000

    form updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import Dictionary from "./Dictionary"
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header text-center">
         <h1 className="app-logo fw-bold">Dictionary</h1>
-      </header>     
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="footer-links text-center">Coded by Kate Kim.</footer>     
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./Dictionary.css";
+
+
+export default function Dictionary () {
+
+    const [keyword, setKeyword] = useState("");
+
+    function handleKeywordChange(e) {
+        setKeyword(e.target.value);
+    }
+
+    function search(e) {
+        e.preventDefault();
+        if(keyword) {
+            alert('you are searching "' + keyword + '" right now.');
+        }
+    }
+    return (
+        <div>
+            <form 
+                className="d-flex flex-column align-items-center p-4 flex-md-row Form"
+                onSubmit={search}
+            >
+                <input 
+                    type="search" 
+                    className="form-control m-2 border-0 shadow" 
+                    placeholder="Search word" 
+                    autoFocus={true} 
+                    onChange={handleKeywordChange}
+                />
+                <button className="btn text-dark form-control m-2 searchBtn shadow fw-bold text-dark">Search</button>                
+            </form>
+        </div>
+    )
+}
