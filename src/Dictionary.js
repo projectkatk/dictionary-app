7a1b8eb4c46c7a08c8d7c599a132f044eb24d523
Kate Kim <kay.k.projects@gmail.com>
2022-03-09 00:40:35 +1000

    API search function updated

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios"
 
 
 export default function Dictionary () {
@@ -10,11 +11,19 @@ export default function Dictionary () {
         setKeyword(e.target.value);
     }
 
+
+    function handleResponse(response) {
+        console.log(response.data[0])
+    }
+
     function search(e) {
         e.preventDefault();
         if(keyword) {
-            alert('you are searching "' + keyword + '" right now.');
+            axios
+                .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`)
+                .then(handleResponse)
         }
+        document.querySelector('.meaning-section').style.visibility = 'visible';
     }
     return (
         <div>
@@ -31,6 +40,23 @@ export default function Dictionary () {
                 />
                 <button className="btn text-dark form-control m-2 searchBtn shadow fw-bold text-dark">Search</button>                
             </form>
+            <section className="p-4 bg-white meaning-section">
+                <div className="row py-2 px-md-4 h-auto d-flex flex-column inner-border mx-md-2">
+                    <div className="col d-flex align-items-center">
+                        <h1 className="px-2">{keyword}</h1>
+                        <span className="d-block px-4">phonetic</span>                        
+                    </div>
+                    <span className="border border-2 p-1 mx-3 wordType mt-2 text-center">adjective</span>
+                    <div className="col">
+
+                    </div>
+
+                    
+
+                </div>
+                
+                
+            </section>
         </div>
     )
 }
